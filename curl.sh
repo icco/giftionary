c94b8d3e2ed2e9d9c79bdeedca0332791a900292
Nat Welch <nat@natwelch.com>
2015-06-04 19:04:54 +0000

    still works

--- curl.sh
+++ curl.sh
@@ -6,7 +6,7 @@ curl 'https://www.tumblr.com/svc/search/inline_gif' \
 -H 'accept-encoding: gzip, deflate' \
 -H 'accept-language: en-US,en;q=0.8' \
 -H 'x-requested-with: XMLHttpRequest' \
--H 'cookie: _springMet=%7B%22uid%22%3A%221Qk5dIx8GHNKJ.8%22%2C%22last%22%3A1340996905168%2C%22step%22%3A1%2C%22cqtime%22%3A1340996919823%2C%22cqurl%22%3A%22%22%7D; tmgioct=kqJB1eIflsDoaQEb5APVkyK5; s_fid=6F358DD4EB94308F-235D8AEC5C202C71; anon_id=OBDELJEEBZSTYVPMPJTNONRPSPAXBTKL; pfp=ifMVfdKh5mrfxBApxKuxRbtz5QLe7GHieTUwgrMz; pfs=6UtIqP0FILNPClJ8eD5ggwmdV8; pfe=1441213499; pfu=120853812' \
+-H 'cookie: pfp=ifMVfdKh5mrfxBApxKuxRbtz5QLe7GHieTUwgrMz; pfs=6UtIqP0FILNPClJ8eD5ggwmdV8; pfe=1441213499; pfu=120853812' \
 -H 'pragma: no-cache' \
 -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36' \
 -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
