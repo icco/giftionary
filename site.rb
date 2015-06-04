d5b7aebf16be0827f491d731a19e1303797c3b7f
Nat Welch <nat@natwelch.com>
2015-06-04 19:51:28 +0000

    site

--- site.rb
+++ site.rb
@@ -1,22 +1,39 @@
-#! /usr/bin/env ruby
+require "rubygems"
+require "bundler"
+Bundler.require(:default, ENV["RACK_ENV"] || :development)
 
-require "faraday"
-require "json"
-
-conn = Faraday.new(:url => "https://www.tumblr.com") do |faraday|
-  faraday.request  :url_encoded             # form-encode POST params
-  faraday.response :logger                  # log requests to STDOUT
-  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+configure do
+  RACK_ENV = (ENV['RACK_ENV'] || :development).to_sym
 end
 
-response = conn.post do |req|
-  req.url "/svc/search/inline_gif"
-  req.headers["cookie"] = "pfp=ifMVfdKh5mrfxBApxKuxRbtz5QLe7GHieTUwgrMz; pfs=6UtIqP0FILNPClJ8eD5ggwmdV8; pfe=1441213499; pfu=120853812"
-  req.body = 'q=&limit=200'
+get "/" do
+  erb :index
 end
 
-data = JSON.parse response.body
+post "/" do
+  redirect "/s/#{params["q"]}"
+end
 
-data["response"]["media"].each do |pic|
-  puts pic["media_url"]
+get "/s/:query" do
+  @urls = query(params[:query])
+  erb :images
+end
+
+def query str
+  conn = Faraday.new(:url => "https://www.tumblr.com") do |faraday|
+    faraday.request  :url_encoded
+    faraday.response :logger
+    faraday.adapter  Faraday.default_adapter
+  end
+  response = conn.post do |req|
+    req.url "/svc/search/inline_gif"
+    req.headers["cookie"] = "pfp=ifMVfdKh5mrfxBApxKuxRbtz5QLe7GHieTUwgrMz; pfs=6UtIqP0FILNPClJ8eD5ggwmdV8; pfe=1441213499; pfu=120853812"
+    req.body = 'q=&limit=200'
+  end
+
+  data = JSON.parse response.body
+
+  return data["response"]["media"].map do |pic|
+    pic["media_url"]
+  end
 end
