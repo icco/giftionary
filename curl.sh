57871730dadb51bc5ec8e4a1de86b5ef1254edd9
Nat Welch <nat@natwelch.com>
2015-06-04 19:04:07 +0000

    still works

--- curl.sh
+++ curl.sh
@@ -6,7 +6,7 @@ curl 'https://www.tumblr.com/svc/search/inline_gif' \
 -H 'accept-encoding: gzip, deflate' \
 -H 'accept-language: en-US,en;q=0.8' \
 -H 'x-requested-with: XMLHttpRequest' \
--H 'cookie: __qca=P0-763584570-1327187375279; _springMet=%7B%22uid%22%3A%221Qk5dIx8GHNKJ.8%22%2C%22last%22%3A1340996905168%2C%22step%22%3A1%2C%22cqtime%22%3A1340996919823%2C%22cqurl%22%3A%22%22%7D; tmgioct=kqJB1eIflsDoaQEb5APVkyK5; _otor=1350493448666.https%3A//www.facebook.com/; _otui=1040575674.1350493448666.1350493448666.1350493448666.1.1; s_fid=6F358DD4EB94308F-235D8AEC5C202C71; mp_fc3cb5f7efe7c7d1b17e16c1fc4075f6_mixpanel=%7B%22distinct_id%22%3A%20%2214abe6c238f670-0c3399ed1-143e6b5e-13c680-14abe6c239077d%22%2C%22%24search_engine%22%3A%20%22google%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.co.uk%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.co.uk%22%7D; anon_id=OBDELJEEBZSTYVPMPJTNONRPSPAXBTKL; pfp=ifMVfdKh5mrfxBApxKuxRbtz5QLe7GHieTUwgrMz; pfs=6UtIqP0FILNPClJ8eD5ggwmdV8; pfe=1441213499; pfu=120853812' \
+-H 'cookie: _springMet=%7B%22uid%22%3A%221Qk5dIx8GHNKJ.8%22%2C%22last%22%3A1340996905168%2C%22step%22%3A1%2C%22cqtime%22%3A1340996919823%2C%22cqurl%22%3A%22%22%7D; tmgioct=kqJB1eIflsDoaQEb5APVkyK5; s_fid=6F358DD4EB94308F-235D8AEC5C202C71; anon_id=OBDELJEEBZSTYVPMPJTNONRPSPAXBTKL; pfp=ifMVfdKh5mrfxBApxKuxRbtz5QLe7GHieTUwgrMz; pfs=6UtIqP0FILNPClJ8eD5ggwmdV8; pfe=1441213499; pfu=120853812' \
 -H 'pragma: no-cache' \
 -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36' \
 -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
