7a88ec137d468f4cea70befff391c23c471bff37
Nat Welch <nat@natwelch.com>
2015-06-04 19:06:59 +0000

    still works

--- curl.sh
+++ curl.sh
@@ -1,16 +1,8 @@
 #! /bin/bash
 
 curl 'https://www.tumblr.com/svc/search/inline_gif' \
--H 'x-tumblr-form-key: TZDa1ozE8d8ebfft06sPZakAypM' \
 -H 'origin: https://www.tumblr.com' \
--H 'accept-encoding: gzip, deflate' \
--H 'accept-language: en-US,en;q=0.8' \
 -H 'x-requested-with: XMLHttpRequest' \
 -H 'cookie: pfp=ifMVfdKh5mrfxBApxKuxRbtz5QLe7GHieTUwgrMz; pfs=6UtIqP0FILNPClJ8eD5ggwmdV8; pfe=1441213499; pfu=120853812' \
--H 'pragma: no-cache' \
--H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
--H 'accept: application/json, text/javascript, */*; q=0.01' \
--H 'cache-control: no-cache' \
--H 'referer: https://www.tumblr.com/search/test' \
 --data 'q=&limit=200' \
 --compressed
