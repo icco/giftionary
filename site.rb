6df968309fbff5a24999e9dca85d55d73ed8e0b4
Nat Welch <nat@natwelch.com>
2015-09-16 18:48:58 +0000

    capitalization for shits

--- site.rb
+++ site.rb
@@ -51,8 +51,8 @@ def get_gifs str
     # Actually make the request
     response = conn.post do |req|
       req.url "/svc/search/inline_gif"
-      req.headers["cookie"] = "pfp=gODtDkLrHgUTkhDmdGDlyWZ0eVGMr4aheNJqrtQB; pfs=cAb1Zkgms7RPrw2QbYud0tqm0; pfe=1448636043; pfu=120853812;"
-      req.headers["x-tumblr-form-key"] = "TZDa1ozE8d8ebfft06sPZakAypM"
+      req.headers["Cookie"] = "pfp=gODtDkLrHgUTkhDmdGDlyWZ0eVGMr4aheNJqrtQB; pfs=cAb1Zkgms7RPrw2QbYud0tqm0; pfe=1448636043; pfu=120853812;"
+      req.headers["X-tumblr-form-key"] = "TZDa1ozE8d8ebfft06sPZakAypM"
       req.body = URI.encode_www_form({ q: str, limit: 100 })
     end
 
