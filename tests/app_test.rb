70742e14469df20c3c555ba9265415394ee1f52e
Nat Welch <nat@natwelch.com>
2016-10-12 10:54:00 +0000

    Add a test

--- tests/app_test.rb
+++ tests/app_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path "../test_helper.rb", __FILE__
 
-class Test < Minitest::Test
+class AppTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
@@ -30,6 +30,18 @@ class Test < Minitest::Test
       env =  { 'rack.session' => { username: "test_user" } }
       post "/upload", params, env
       assert last_response.redirect?
+    end
+  end
+
+  def test_upload_ban_stub
+    VCR.use_cassette("upload") do
+      params = {
+        file: Rack::Test::UploadedFile.new(File.expand_path("../flatcircle.gif", __FILE__)),
+        stub: "flat circle"
+      }
+      env =  { 'rack.session' => { username: "test_user" } }
+      post "/upload", params, env
+      refute last_response.ok?
     end
   end
 end
