2faf5af496ca51e362b1d3d76f8c8840fed33528
Nat Welch <nat@natwelch.com>
2016-10-12 10:51:25 +0000

    validate

--- lib/image.rb
+++ lib/image.rb
@@ -1,4 +1,5 @@
 class Image < ActiveRecord::Base
+  validates :stub, format: { with: /\A[a-z\-\_]+\z/, message: "only allows letters, dashes and underbars" }
   def share
     "/#{username}/#{stub}"
   end
