7063fda823b987c7f705c453c0c68bfc75e70687
Nat Welch <nat@natwelch.com>
2017-06-29 19:31:53 +0000

    Allow numbers in stubs

--- lib/image.rb
+++ lib/image.rb
@@ -1,5 +1,6 @@
 class Image < ActiveRecord::Base
-  validates :stub, format: { with: /\A[a-z\-\_]+\z/, message: "only allows letters, dashes and underbars" }
+  validates :stub, format: { with: /\A[a-z\-\_0-9]+\z/, message: "only allows letters, numbers, dashes and underbars" }
+
   def share
     "/#{username}/#{stub}"
   end
